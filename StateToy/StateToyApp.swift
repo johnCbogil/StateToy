3ed1e59920e2c2576dc20e97ac0f6a20a52a31dc
John Bogil <johncbogil@gmail.com>
2023-02-25 15:51:23 -0500

    Initial Commit

---
+++ StateToy/StateToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateToyApp.swift
+//  StateToy
+//
+//  Created by John Bogil on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct StateToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
